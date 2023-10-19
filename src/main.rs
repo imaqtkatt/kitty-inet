c2a77d66f6fb9bf53d5089e353e27ff26124a832
imaqtkatt <eduarda.so@proton.me>
2023-10-18 21:41:12 -0300

    start inet impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,119 @@
+use AgentKind::*;
+
+#[repr(u8)]
+#[derive(Clone, Copy)]
+pub enum AgentKind {
+  Era = 0,
+  Con,
+  Dup,
+}
+
+type AgentId = u8;
+type SlotId = u8;
+
+#[derive(Clone, Copy)]
+pub struct Port(pub AgentId, pub SlotId);
+
+impl Port {
+  pub fn agent(self) -> AgentId {
+    self.0
+  }
+
+  pub fn slot(self) -> SlotId {
+    self.1
+  }
+}
+
+#[derive(Clone, Copy)]
+pub struct Agent {
+  pub main: Port,
+  pub aux1: Port,
+  pub aux2: Port,
+  pub kind: AgentKind,
+}
+
+impl Agent {
+  pub fn new(main: Port, aux1: Port, aux2: Port, kind: AgentKind) -> Self {
+    Self {
+      main,
+      aux1,
+      aux2,
+      kind,
+    }
+  }
+
+  pub fn with_id(id: AgentId, kind: AgentKind) -> Self {
+    Self {
+      main: Port(id, 0),
+      aux1: Port(id, 1),
+      aux2: Port(id, 2),
+      kind,
+    }
+  }
+
+  pub fn port(&self, slot: SlotId) -> Port {
+    match slot {
+      0 => self.main,
+      1 => self.aux1,
+      2 => self.aux2,
+      _ => unreachable!(),
+    }
+  }
+
+  pub fn port_mut(&mut self, slot: SlotId) -> &mut Port {
+    match slot {
+      0 => &mut self.main,
+      1 => &mut self.aux1,
+      2 => &mut self.aux2,
+      _ => unreachable!(),
+    }
+  }
+}
+
+pub struct INet {
+  pub nodes: Vec<Agent>,
+  pub free: Vec<AgentId>,
+}
+
+impl Default for INet {
+  fn default() -> Self {
+    Self {
+      nodes: vec![Agent::new(Port(0, 2), Port(0, 1), Port(0, 0), Era)],
+      free: Default::default(),
+    }
+  }
+}
+
+impl INet {
+  #[inline(always)]
+  pub fn new_id(&mut self) -> AgentId {
+    self.free.pop().unwrap_or(self.nodes.len() as u8)
+  }
+
+  #[inline(always)]
+  pub fn alloc(&mut self, kind: AgentKind) -> AgentId {
+    let id = self.new_id();
+    self.nodes[id as usize] = Agent::with_id(id, kind);
+    id
+  }
+
+  #[inline(always)]
+  pub fn free(&mut self, id: AgentId) {
+    self.free.push(id)
+  }
+
+  #[inline(always)]
+  pub fn link(&mut self, a: Port, b: Port) {
+    *self.nodes[a.agent() as usize].port_mut(a.slot()) = b;
+    *self.nodes[b.agent() as usize].port_mut(b.slot()) = a;
+  }
+
+  #[inline(always)]
+  pub fn enter(&mut self, port: Port) -> Port {
+    self.nodes[port.agent() as usize].port(port.slot())
+  }
+}
+
 fn main() {
   println!("Hello, world!");
 }
